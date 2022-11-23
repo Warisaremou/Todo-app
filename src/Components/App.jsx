27b96da1aecfcd5cfa9c11b8b5bd9e0ed593d0e6
warisaremou <warisaremou2003@gmail.com>
2022-11-23 17:22:11 +0100

    Setting up my project with reactjs and tailwindcss

---
+++ src/Components/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="font-poppins">
+      <h1 className="text-3xl text-blue-500 font-bold text-center mt-4">Create  a new todo...</h1>
+    </div>
+  );
+}
+
+export default App;
