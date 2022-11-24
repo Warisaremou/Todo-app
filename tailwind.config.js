3b8c656f2d54819accdf976b8df8c750de3ed861
warisaremou <warisaremou2003@gmail.com>
2022-11-24 19:54:43 +0100

    Adding darkMode and changing the font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     ],
   theme: {
     extend: {},
     fontFamily: {
-      poppins: ["Caudex, serif"],
+      caudex: ["Caudex, serif"],
     },
   },
   plugins: [],
