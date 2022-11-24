f7eb70ffdd0f49654d8963a074a88e54e4097cbd
warisaremou <warisaremou2003@gmail.com>
2022-11-24 20:36:35 +0100

    Setting up useState to add input value to my todo list

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header/Header";
 import TodoList from "./TodoList/TodoList";
+import { useState } from "react";
 import mobileBg from "../Images/bg-mobile-light.jpg"
 import desktopBg from "../Images/bg-desktop-light.jpg"
 import MatchMediaWrapper from "../js/MatchMediaWrapper";
@@ -9,11 +10,14 @@ function App() {
   const mobileBackground = (<img src={mobileBg} alt="mobile-bg" className="absolute top-0 left-0 right-0 -z-10 w-full" />)
   const desktopBackground = (<img src={desktopBg} alt="Desktop-bg" className="absolute top-0 left-0 right-0 -z-10 w-full" />)
 
+  const [inputValue, setInputValue] = useState('')
+  const [todo, setTodo] = useState([])
+
   return (
     <div className="font-caudex relative">
       <div className="max-w-screen-md mx-auto pt-5 px-5 md:px-  14">
-        <Header />
-        <TodoList />
+        <Header inputValue={inputValue} setInputValue={setInputValue} setTodo={setTodo} />
+        <TodoList todo={todo} setTodo={setTodo} />
       </div>
       <div className="layout">
         <MatchMediaWrapper mobileBackground={mobileBackground} desktopBackground={desktopBackground} />
