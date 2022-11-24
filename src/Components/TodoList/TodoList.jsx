7a74fcfbc5b2d813e2ce8ccfea42c0a1c7d3ea0d
warisaremou <warisaremou2003@gmail.com>
2022-11-24 20:40:13 +0100

    Getting todo input by props and adding them to my todo list

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -2,27 +2,20 @@ import React from 'react'
 import { Check } from "react-bootstrap-icons"
 // import { Home } from "react-bootstrap-icons"
 
-function TodoList() {
+function TodoList({ todo, setTodo }) {
     return (
         <div className='bg-white rounded-lg shadow-lg'>
-            <div className="todo py-5 border-b-2">
-                <div className="px-3 flex justify-between items-center">
-                    <div className='flex items-center'>
-                        <Check className='check-btn' />
-                        <p className='text-gray-700 md:text-lg pl-5'>Complete online JavaScript course</p>
+            {todo.map((myTodo, index) => (
+                <div key={`${myTodo}-${index}`} className="todo py-5 border-b-2">
+                    <div className="px-3 flex justify-between items-center">
+                        <div className='flex items-center'>
+                            <Check className='check-btn' />
+                            <p className='text-gray-700 md:text-lg pl-5'>{myTodo.charAt(0).toUpperCase() + myTodo.slice(1)}</p>
+                        </div>
+                        <Check className='clear-btn' />
                     </div>
-                    <Check className='clear-btn' />
                 </div>
-            </div>
-            <div className="todo py-5 border-b-2">
-                <div className="px-3 flex justify-between items-center">
-                    <div className='flex items-center'>
-                        <Check className='check-btn' />
-                        <p className='text-gray-700 md:text-lg pl-5'>Jog around the park 3x</p>
-                    </div>
-                    <Check className='clear-btn' />
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
