55c6faee3db916c57b50336e3fe63dd042db71d0
warisaremou <warisaremou2003@gmail.com>
2022-11-24 20:00:17 +0100

    Adding Header, TodoList and MatchMediaWrapper components

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,23 @@
+import Header from "./Header/Header";
+import TodoList from "./TodoList/TodoList";
+import mobileBg from "../Images/bg-mobile-light.jpg"
+import desktopBg from "../Images/bg-desktop-light.jpg"
+import MatchMediaWrapper from "../js/MatchMediaWrapper";
 
 function App() {
+
+  const mobileBackground = (<img src={mobileBg} alt="mobile-bg" className="absolute top-0 left-0 right-0 -z-10 w-full" />)
+  const desktopBackground = (<img src={desktopBg} alt="Desktop-bg" className="absolute top-0 left-0 right-0 -z-10 w-full" />)
+
   return (
-    <div className="font-poppins">
-      <h1 className="text-3xl text-blue-500 font-bold text-center mt-4">Create  a new todo...</h1>
+    <div className="font-caudex relative">
+      <div className="max-w-screen-md mx-auto pt-5 px-5 md:px-  14">
+        <Header />
+        <TodoList />
+      </div>
+      <div className="layout">
+        <MatchMediaWrapper mobileBackground={mobileBackground} desktopBackground={desktopBackground} />
+      </div>
     </div>
   );
 }
