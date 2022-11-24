293d84c6ce85a3ba031e704241f01d5ab1429dca
warisaremou <warisaremou2003@gmail.com>
2022-11-24 20:38:16 +0100

    Passing props and  adding function to get input value and add them to my todo list

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { BrightnessLowFill } from "react-bootstrap-icons"
 import { MoonFill } from "react-bootstrap-icons"
 
-function Header() {
+function Header({ inputValue, setInputValue, setTodo }) {
+
+    function keyPressed(e) {
+        if (e.keyCode === 13) {
+            console.log(inputValue)
+            setTodo(currentInput => [...currentInput, inputValue])
+            setInputValue('')
+        }
+    }
+
     return (
         <div className='py-5'>
             <div className="text-2xl md:text-4xl text-white flex justify-between mb-9">
@@ -10,7 +19,7 @@ function Header() {
                 <BrightnessLowFill className="cursor-pointer hidden" />
                 <MoonFill className="cursor-pointer md:text-3xl" />
             </div>
-            <input type="text" className='w-full h-11 px-3 py-8 rounded-lg outline-blue-500 text-lg md:text-xl text-gray-700' placeholder='Create a new todo...' />
+            <input type="text" value={inputValue} className='todo-input' placeholder='Create a new todo...' onChange={(e) => setInputValue(e.target.value)} onKeyDown={keyPressed} />
         </div>
     )
 }
