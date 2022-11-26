c382061e3da4fd9202d827b962fc73e5d5a30bc8
warisaremou <warisaremou2003@gmail.com>
2022-11-26 20:04:06 +0100

    Adding handleThemeSwitch function to my app and toogling sun/moon components

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrightnessLowFill } from "react-bootstrap-icons"
 import { MoonFill } from "react-bootstrap-icons"
 
-function Header({ inputValue, setInputValue, setTodo }) {
+function Header({ inputValue, setInputValue, setTodo, theme, setTheme }) {
 
     function keyPressed(e) {
         if (e.keyCode === 13) {
@@ -12,12 +12,19 @@ function Header({ inputValue, setInputValue, setTodo }) {
         }
     }
 
+    const handleThemeSwitch = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
     return (
         <div className='py-5'>
-            <div className="text-2xl md:text-4xl text-white flex justify-between mb-9">
-                <h3 className='font-extrabold items-center'>TODO</h3>
-                <BrightnessLowFill className="cursor-pointer hidden" />
-                <MoonFill className="cursor-pointer md:text-3xl" />
+            <div className="text-2xl md:text-4xl text-white flex justify-between mb-9 items-center">
+                <h3 className='font-extrabold tracking-widest'>TODO</h3>
+                {theme === 'light' ? (
+                    <BrightnessLowFill className="cursor-pointer toogle-btn" onClick={handleThemeSwitch} />
+                ) : (
+                    <MoonFill className="cursor-pointer md:text-3xl toogle-btn" onClick={handleThemeSwitch} />
+                )}
             </div>
             <input type="text" value={inputValue} className='todo-input' placeholder='Create a new todo...' onChange={(e) => setInputValue(e.target.value)} onKeyDown={keyPressed} />
         </div>
