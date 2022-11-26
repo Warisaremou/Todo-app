1417171ca1352f38be9f0da384d59ac58902d410
warisaremou <warisaremou2003@gmail.com>
2022-11-26 17:17:25 +0100

    Adding useEffect to storage my todo list

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import TodoList from "./TodoList/TodoList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import mobileBg from "../Images/bg-mobile-light.jpg"
 import desktopBg from "../Images/bg-desktop-light.jpg"
 import MatchMediaWrapper from "../js/MatchMediaWrapper";
@@ -10,11 +10,16 @@ function App() {
   const mobileBackground = (<img src={mobileBg} alt="mobile-bg" className="absolute top-0 left-0 right-0 -z-10 w-full" />)
   const desktopBackground = (<img src={desktopBg} alt="Desktop-bg" className="absolute top-0 left-0 right-0 -z-10 w-full" />)
 
+  const savedTodo = localStorage.getItem('todos')
+  const [todo, setTodo] = useState(savedTodo ? JSON.parse(savedTodo) : [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todo))
+  }, [todo])
   const [inputValue, setInputValue] = useState('')
-  const [todo, setTodo] = useState([])
 
   return (
-    <div className="font-caudex relative">
+    <div className="font-josefin relative">
       <div className="max-w-screen-md mx-auto pt-5 px-5 md:px-  14">
         <Header inputValue={inputValue} setInputValue={setInputValue} setTodo={setTodo} />
         <TodoList todo={todo} setTodo={setTodo} />
