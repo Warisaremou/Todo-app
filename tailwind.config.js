2486118249debf7b37c12dfd6a2a780b1694b01c
warisaremou <warisaremou2003@gmail.com>
2022-11-26 19:58:08 +0100

    Adding style to my app

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,23 @@ module.exports = {
   darkMode: "class",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    ],
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-body': '#FAFAFA',
+        'dark-bg': '#161722',
+        'input-dark': '#25273C',
+        'text-light': '#FAFAFA',
+        'text-dark': '#484B6A',
+        'light-shadow': '#E4E5F1',
+        'dark-shadow': '#181818',
+        'active-option': '#3A7BFD',
+      },
+    },
     fontFamily: {
-      josefin: ["Josefin Sans, sans-serif"],
+      josefin: ['Josefin Sans, sans-serif'],
+      poppins: ['Poppins, sans-serif'],
     },
   },
   plugins: [],
