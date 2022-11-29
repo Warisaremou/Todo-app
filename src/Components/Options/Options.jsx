2eaaaecb59d130e42d43208e4c8d2ab45c9a6181
warisaremou <warisaremou2003@gmail.com>
2022-11-29 16:22:52 +0100

    Adding main-options class

--- src/Components/Options/Options.jsx
+++ src/Components/Options/Options.jsx
@@ -7,9 +7,9 @@ function Options({ todo, setTodo }) {
     }
 
     return (
-        <div className='text-light-gray text-lg p-5 flex justify-between'>
+        <div className='text-light-gray text-lg p-5 flex justify-between relative'>
             <span>{`${todo.length} items left`}</span>
-            <div className='font-semibold'>
+            <div className='main-options'>
                 <button className='text-active-option'>All</button>
                 <button className='px-5 options'>Active</button>
                 <button className='options'>Completed</button>
