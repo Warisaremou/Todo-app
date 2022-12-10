c8abcadf66cd0af0ad0a540e5bb96695556cf78e
warisaremou <warisaremou2003@gmail.com>
2022-12-10 21:25:04 +0100

    Updating onClick event and handleThemeSwitch function

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -16,24 +16,20 @@ function Header({ inputValue, setInputValue, setTodo, theme, setTheme }) {
   }
 
   const handleThemeSwitch = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+    setTheme(theme === "dark" ? "light" : "dark");
   };
 
   return (
     <div className="py-5">
       <div className="text-2xl md:text-4xl text-white flex justify-between mb-9 items-center">
         <h3 className="font-extrabold tracking-widest">TODO</h3>
-        {theme === "light" ? (
-          <BrightnessLowFill
-            className="cursor-pointer text-3xl toogle-btn"
-            onClick={handleThemeSwitch}
-          />
-        ) : (
-          <MoonFill
-            className="cursor-pointer md:text-3xl toogle-btn"
-            onClick={handleThemeSwitch}
-          />
-        )}
+        <div onClick={handleThemeSwitch}>
+          {theme === "light" ? (
+            <BrightnessLowFill className="cursor-pointer text-3xl toogle-btn" />
+          ) : (
+            <MoonFill className="cursor-pointer md:text-3xl toogle-btn" />
+          )}
+        </div>
       </div>
       <input
         type="text"
